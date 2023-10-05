81883dbcc71f1a98d6f477c67a1024a07300b724
fillah firdausyah <fillah.akbar@viapulsa.id>
2023-10-05 09:38:22 +0700

    fix: change socket server url

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.WEBSOCKET_URL;
+// const URL = process.env.WEBSOCKET_URL;
+const URL = 'https://live-stream-rtc-backend.vercel.app/';
 
 export const socket = io(URL, { autoConnect: false });
