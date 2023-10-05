cfd01ab575beaeb9b30d4efb8aa1ecbba993a71e
fillah firdausyah <fillah.akbar@viapulsa.id>
2023-10-05 15:11:58 +0700

    change socket url

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // const URL = process.env.WEBSOCKET_URL;
-const URL = 'http://54.254.186.51:5000/';
+const URL = 'http://13.250.57.155:5000/';
 
 export const socket = io(URL, { autoConnect: false });
